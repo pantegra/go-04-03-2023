3c1bd92e39a106e8a2cd306948b3c184f402ee9e
pantegra <brian@dashley.net>
2023-04-03 16:57:30 +0000

    new files

---
+++ goBasicChallange.go
@@ -0,0 +1,14 @@
+
+package main 
+
+import "fmt"
+
+func main() {
+
+    var name = "Brian Erickson"
+    var city, state = "Hagerman", "New Mexico"
+
+    fmt.Println("My Name is:", name)
+    fmt.Println("I live in ", city , ",", state)
+}
+
